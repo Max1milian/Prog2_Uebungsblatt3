96470696edeb0d0d34ccb5b809a123ea8a2ebf4b
Maximilian <max.stocker@gmx.de>
2022-05-06 12:37:54 +0200

    initial commit

---
+++ src/Position.cpp
@@ -0,0 +1,29 @@
+#include "Position.h"
+int Position::getX()
+{
+	return x;
+}
+void Position::setX(int x)
+{
+	//IHR CODE
+}
+int Position::getY()
+{
+	return y;
+}
+void Position::setY(int y)
+{
+	//IHR CODE
+}
+//Addiert zwei Positionen indem die Attribute x und y addiert werden.
+
+Position Position::operator+ (const Position& rhs) const {
+	//IHR CODE
+	return Position(0,0);
+}
+//Vergleicht zwei Position Instanzen. Gibt true zurück, falls x und y beider Instanzen identisch sind, sonst false.
+bool Position::operator==(const  Position& rhs) const
+{
+	//IHR CODE
+	return true;
+}
---
+++ src/Spielstein.cpp
@@ -0,0 +1,183 @@
+#include "Spielstein.h"
+
+void Spielstein::init() {
+	//IHR CODE / TEILVORLAGE
+	switch (typ)
+	{
+	case SPIELSTEIN_Z2:
+	{
+		name = "Z2";
+		farbe = HINTERGRUND_BLAU;
+		felder[0][0].push_back(Position(2, 0));//__#_
+		felder[0][0].push_back(Position(1, 1));//_##_
+		felder[0][0].push_back(Position(2, 1));//_#__
+		felder[0][0].push_back(Position(1, 2));//____
+
+		felder[0][1].push_back(Position(1, 1));//____
+		felder[0][1].push_back(Position(2, 1));//_##_
+		felder[0][1].push_back(Position(2, 2));//__##
+		felder[0][1].push_back(Position(3, 2));//____
+
+		felder[0][2].push_back(Position(2, 0));//__#_
+		felder[0][2].push_back(Position(1, 1));//_##_
+		felder[0][2].push_back(Position(2, 1));//_#__
+		felder[0][2].push_back(Position(1, 2));//____
+
+		felder[0][3].push_back(Position(1, 1));//____
+		felder[0][3].push_back(Position(2, 1));//_##_
+		felder[0][3].push_back(Position(2, 2));//__##
+		felder[0][3].push_back(Position(3, 2));//____
+
+		felder[1][0].push_back(Position(1, 0));//_#__
+		felder[1][0].push_back(Position(1, 1));//_##_
+		felder[1][0].push_back(Position(2, 1));//__#_
+		felder[1][0].push_back(Position(2, 2));//____
+
+		felder[1][1].push_back(Position(2, 1));//____
+		felder[1][1].push_back(Position(3, 1));//__##
+		felder[1][1].push_back(Position(1, 2));//_##_
+		felder[1][1].push_back(Position(2, 2));//____
+
+		felder[1][2].push_back(Position(1, 0));//_#__
+		felder[1][2].push_back(Position(1, 1));//_##_
+		felder[1][2].push_back(Position(2, 1));//__#_
+		felder[1][2].push_back(Position(2, 2));//____
+
+		felder[1][3].push_back(Position(2, 1));//____
+		felder[1][3].push_back(Position(3, 1));//__##
+		felder[1][3].push_back(Position(1, 2));//_##_
+		felder[1][3].push_back(Position(2, 2));//____
+		break;
+	}
+	case SPIELSTEIN_Z3:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_L2:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_L3:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_L4:
+	{
+		
+
+		break;
+	}
+	case SPIELSTEIN_I2:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_I3:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_I4:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_Q:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_QPLUS: //GECHECKT
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_T:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_TPLUS:
+	{
+		
+		break;
+	}
+	case SPIELSTEIN_FREI:
+	{
+		name = "FREI";
+		farbe = HINTERGRUND_HELL_GRAU;
+		break;
+	}
+	case SPIELSTEIN_LOESUNG:
+	{
+		name = "LOESUNG";
+		farbe = HINTERGRUND_HELL_WEISS;
+		break;
+	}
+	default:
+		break;
+	}
+}
+void Spielstein::zeichne()
+{
+	//IHR CODE
+}
+void Spielstein::loesche()
+{
+	//IHR CODE
+}
+
+
+void Spielstein::bewegen(int richtung)
+{
+	//IHR CODE
+}
+//Inkrementiert das Attribut orientierung um 1, wobei stets Werte zwischen 0 und 3 angenommen werden.
+void Spielstein::rotation_rechts()
+{
+	//IHR CODE
+}
+//Dekrementiert das Attribut orientierung um 1, wobei stets Werte zwischen 0 und 3 angenommen werden.
+void Spielstein::rotation_links()
+{
+	//IHR CODE
+}
+//Inkrementiert das Attribut seite um 1, wobei stets Werte zwischen 0 und 1 angenommen werden.
+void Spielstein::flip()
+{
+	//IHR CODE
+}
+
+//Gibt den felder Vektor der aktuellen Lage des Spielsteins zurück.
+std::vector<Position>* Spielstein::getPositionen()
+{
+	//IHR CODE
+	return null;
+}
+
+//Durchläuft den felder Vektor des Spielsteins in aktueller Lage.
+//Gibt true zurück, falls die übergebene Position innerhalb des Spielsteines liegt.
+//Ansonsten wird false zurückgegeben.
+bool Spielstein::innerhalb(Position pos) const
+{
+	//IHR CODE
+	return true;
+}
+
+//Gibt war zurück, falls sich der aufrufende und der übergebene Spielstein sich in mindestens einer Position überlappen.
+bool Spielstein::ueberlapp(Spielstein& sp) 
+{
+	//IHR CODE
+	return true;
+}
+
+//Addiert zwei Spielsteine in ihrer aktuellen Lage und gibt die Summe als neuen Spielstein zurück.
+Spielstein Spielstein::operator+ (const Spielstein& rhs) const
+{
+	Spielstein summe(SPIELSTEIN_FREI);
+	//IHR CODE
+	return summe;
+}
